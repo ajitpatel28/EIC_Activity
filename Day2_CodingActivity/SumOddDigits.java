418e6dc1e3798624d596f671cfa2ecac55eb8e64
Ajit.Patel <ajit.patel@einfochips.com>
2023-02-09 10:03:56 +0530

    updating SumOddDigits.java

--- Day2_CodingActivity/SumOddDigits.java
+++ Day2_CodingActivity/SumOddDigits.java
@@ -9,7 +9,7 @@ public class SumOddDigits{
         }
         
          else if(n>32767){
-            System.out.println("Number too large");
+            System.out.println("Invalid Input");
         }
         
         else{
@@ -17,16 +17,16 @@ public class SumOddDigits{
 
               String str = n.toString();
 
-    //convert String to char array
+    
     char[] chAr = str.toCharArray();        
     int sum = 0;
     for(int i=0;i<chAr.length;i++){
 
 
-    //convert char array to integer inorder to verify 'if it is a odd number or not?'
+
     int a = Integer.parseInt(String.valueOf(chAr[i]));
 
-    //if a is odd, then add 'a' to sum
+    
     if(a%2==1) {
         sum = sum + a;
     }
@@ -34,19 +34,6 @@ public class SumOddDigits{
     }
 
     System.out.println(sum);
-
-            // int sum = 0;
-            //         int i = 1;
-
-            // while(n!=0){
-            //     if(i%2 != 0){
-            //         sum += i;
-            //     }
-            //     i++;
-              
-                
-            // }
-            //        System.out.println(sum);
             }
           
         }
